9f82f77c5f45f6036b73db4b453f029c72bfac42
Classique-Smokes <gabav99@gmail.com>
2026-09-22 15:00:24 -0300

    test: reproduce IMP-0001 F1-F3 conformance blockers

--- tests/Mesopotamia.Sim.AcceptanceTests/Scenarios.cs
+++ tests/Mesopotamia.Sim.AcceptanceTests/Scenarios.cs
@@ -28,6 +28,7 @@ internal sealed partial class Scenarios
         foreach (Scenario scenario in AgencyCases()) yield return scenario;
         foreach (Scenario scenario in CrossCuttingCases()) yield return scenario;
         foreach (Scenario scenario in AttitudeEventCases()) yield return scenario;
+        foreach (Scenario scenario in RepairCases()) yield return scenario;
         yield return new("ConsumptionAndFarm", ["S1-090-A", "S1-090-B", "S1-090-C", "S1-090-E"], () =>
         {
             Simulation sim = Create(World(1));
